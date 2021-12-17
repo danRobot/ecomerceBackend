db4bb24de637033ededb912c769af99597139c04
daniel <danielgallo502@gmail.com>
2021-12-17 18:39:23 -0500

    first clean commit

---
+++ src/main/java/com/ciclo4/app/EcomerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo4.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceBackendApplication.class, args);
+	}
+
+}
